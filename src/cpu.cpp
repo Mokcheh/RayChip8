c1982b31a7bcae7d3f8b653ebbfae4dc993536aa
Mokcheh <mohamedmokcheh@protonmail.com>
2021-09-09 17:34:57 +0100

    eh

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -8,6 +8,14 @@ unsigned short Vy;
 unsigned short N; //height
 unsigned short pixel;
 int i;
+template <typename I> std::string hex(I w, size_t hex_len = sizeof(I)<<1) {
+    static const char* digits = "0123456789ABCDEF";
+    std::string rc(hex_len,'0');
+    for (size_t i=0, j=(hex_len-1)*4 ; i<hex_len; ++i,j-=4)
+        rc[i] = digits[(w>>j) & 0x0f];
+    return rc;
+}
+
 
 
 int fsize(FILE *fp){
